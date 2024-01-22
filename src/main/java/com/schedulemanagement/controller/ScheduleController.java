f938a37d21680b4e4a386bfae12937f300e211b0
jinkshower <askthem@naver.com>
2024-01-22 16:16:14 +0900

    feat: implement basic http method

--- src/main/java/com/schedulemanagement/controller/ScheduleController.java
+++ src/main/java/com/schedulemanagement/controller/ScheduleController.java
@@ -1,4 +1,34 @@
 package com.schedulemanagement.controller;
 
+import com.schedulemanagement.dto.ScheduleResponseDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/schedule")
 public class ScheduleController {
+
+    @PostMapping
+    public String createSchedule() {
+        return "post";
+    }
+
+    @GetMapping
+    public String getSchedules() {
+        return "default get";
+    }
+
+    @GetMapping("/{id}")
+    public String getSchedule(@PathVariable Long id) {
+        return id + "get";
+    }
+
+    @PutMapping("/{id}")
+    public String updateSchedule(@PathVariable Long id) {
+        return id + "put";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteSchedule(@PathVariable Long id) {
+        return id + "delete";
+    }
 }
