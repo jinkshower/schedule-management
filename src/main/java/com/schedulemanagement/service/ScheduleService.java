090cf5d813e4902eccd8659e9ef8e74438c830ef
jinkshower <askthem@naver.com>
2024-02-01 10:30:41 +0900

    .

--- src/main/java/com/schedulemanagement/service/ScheduleService.java
+++ src/main/java/com/schedulemanagement/service/ScheduleService.java
@@ -6,7 +6,6 @@ import com.schedulemanagement.entity.Schedule;
 import com.schedulemanagement.repository.ScheduleRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
